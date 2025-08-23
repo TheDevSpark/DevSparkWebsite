8cbaa14fbd41557ecc0524d28348109d00bb41e2
“TheDevSpark” <devteam@thedevspark.com>
2025-08-23 14:27:25 -0700

    font add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-inter grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-5xl">Website Coming Soon....</h1>
+        <h1 className="text-5xl font-inter">Website Coming Soon....</h1>
       </main>
     </div>
   );
